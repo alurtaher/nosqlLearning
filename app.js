aa62b4a96d3ce6973a4acd375751f594b73d443d
A Taher Basha <taherbashaengineer@gmail.com>
2025-09-23 13:12:04 +0530

    CRUD operations on mongodb learned

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express");
 let app = express();
 require("dotenv").config();
 let { mongoConnect, getDb } = require("./database/db.js");
+const { MongoClient, ObjectId } = require('mongodb');
 let PORT = process.env.PORT;
 let Product = require("./models/product.js");
 
@@ -19,6 +20,56 @@ app.get("/get-product", async (req, res) => {
   let products =  await db.collection('products').find().toArray();
   return res.status(200).json({ products });
 });
+
+app.get("/get-productbyid/:id",async(req,res)=>{
+    try {
+        let {id} = req.params;
+        const db = getDb();
+        const document = await db.collection('products').findOne({_id:new ObjectId(id)});
+        return res.status(200).json({document})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.put('/update-product/:id', async (req, res) => {
+    try {
+        const db = getDb();
+        const productId = req.params.id;
+
+        let { title, price, description, imageUrl } = req.body;
+
+        let result = await db.collection('products').updateOne(
+            { _id: new ObjectId(productId) },   // filter
+            { $set: { title, price, description, imageUrl } } // updated values
+        )
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+
+        res.status(200).json({ message: "Product updated successfully" })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to update product" })
+    }
+})
+
+app.delete('/delete-product/:id',async(req,res)=>{
+    try {
+        const db = getDb();
+        const productId = req.params.id;
+        let result = await db.collection('products').deleteOne({ _id: new ObjectId(productId) })
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        return res.status(200).json({ message: "Product deleted successfully" })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to delete product" })
+    }
+})
 
 mongoConnect(() => {
   app.listen(PORT, () => {
