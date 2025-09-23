b33349fcee075ab264b07911c9abf6b4f8110578
A Taher Basha <taherbashaengineer@gmail.com>
2025-09-23 12:12:15 +0530

    Read operation

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-let express = require('express')
+let express = require("express");
 let app = express();
-require('dotenv').config()
-let {mongoConnect} = require('./database/db.js')
-let PORT = process.env.PORT
-let Product = require('./models/product.js')
+require("dotenv").config();
+let { mongoConnect, getDb } = require("./database/db.js");
+let PORT = process.env.PORT;
+let Product = require("./models/product.js");
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/add-product',(req,res)=>{
-    let {title,price,description,imageUrl} = req.body;
-    let p1 = new Product(title,price,description,imageUrl)
-    let result = p1.save();
-    res.status(200).json({result})
-})
+app.post("/add-product", async (req, res) => {
+  let { title, price, description, imageUrl } = req.body;
+  let p1 = new Product(title, price, description, imageUrl);
+  let result = await p1.save();
+  res.status(200).json({ result });
+});
 
-mongoConnect(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server is listening on ${PORT}`)
-    })
-})
+app.get("/get-product", async (req, res) => {
+  const db = getDb();
+  let products =  await db.collection('products').find().toArray();
+  return res.status(200).json({ products });
+});
+
+mongoConnect(() => {
+  app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+  });
+});
